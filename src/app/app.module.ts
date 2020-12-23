fe90fe01ffa6d8477f1858a201014fdfbc8008ca
Florian <florian.martin63000@gmail.com>
2020-12-23 11:23:42 +0100

    Feat: add some style to root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { SharedModule } from './shared/shared.module';
-import { DatePipe } from '@angular/common';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
